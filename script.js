ca8bd32ec98771749934b0ad32a7389fa452c831
Stanislav Minko <minkostas@gmail.com>
2021-12-24 01:52:22 +0300

    feat: move right, left, up & down

--- script.js
+++ script.js
@@ -74,10 +74,34 @@ function move() {
     snakeBody[snakeBody.length-1].classList.remove('snakeBody');
     snakeBody.pop();
 
-    if(snakeCoordinates[0] < 10) {
-        snakeBody.unshift(document.querySelector('[posX = "' + (+snakeCoordinates[0] + 1) + '"][posY = "' + snakeCoordinates[1] + '"]'));
-    } else {
-        snakeBody.unshift(document.querySelector('[posX = "1"][posY = "' + snakeCoordinates[1] + '"]'));
+    if (direction == 'right') {
+        if(snakeCoordinates[0] < 10) {
+            snakeBody.unshift(document.querySelector('[posX = "' + (+snakeCoordinates[0] + 1) + '"][posY = "' + snakeCoordinates[1] + '"]'));
+        } else {
+            snakeBody.unshift(document.querySelector('[posX = "1"][posY = "' + snakeCoordinates[1] + '"]'));
+        }
+        
+    } else if (direction == 'left') {
+        if(snakeCoordinates[0] > 1) {
+            snakeBody.unshift(document.querySelector('[posX = "' + (+snakeCoordinates[0] - 1) + '"][posY = "' + snakeCoordinates[1] + '"]'));
+        } else {
+            snakeBody.unshift(document.querySelector('[posX = "10"][posY = "' + snakeCoordinates[1] + '"]'));
+        }
+        
+    } else if (direction == 'up') {
+        if(snakeCoordinates[1] < 10) {
+            snakeBody.unshift(document.querySelector('[posX = "'+ snakeCoordinates[0] + '"][posY = "'+(+snakeCoordinates[1] + 1) +'"]'));
+        } else {
+            snakeBody.unshift(document.querySelector('[posX = "'+ snakeCoordinates[0] + '"][posY = "1"]'));
+        }
+        
+    } else if (direction == 'down') {
+        if(snakeCoordinates[1] > 1) {
+            snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "' + (snakeCoordinates[1]-1) + '"]'));
+        } else {
+            snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "10"]'));
+        }
+        
     }
 
     snakeBody[0].classList.add('head');
@@ -90,15 +114,15 @@ let interval = setInterval(move, 300);
 
 window.addEventListener('keydown', function (e) {
     if (e.keyCode == 37 && direction!= 'right') {
-        console.log('true');
+        direction = 'left';
     }
     else if(e.keyCode == 38 && direction!= 'down') {
-        console.log('true');
+        direction = 'up';
     }
     else if(e.keyCode == 39 && direction!= 'left') {
-        console.log('true');
+        direction = 'right';
     }
     else if(e.keyCode == 40 && direction!= 'up') {
-        console.log('true');
+        direction = 'down';
     }
 });
